544032b4f8c507c914508f9b8e61af093c5339e5
nishitani yuki <yuki.nishitani85@gmail.com>
2016-12-04 09:32:07 +0900

    add redux-thunk middle ware to store

--- src/js/store/createStoreWithMiddleware.dev.js
+++ src/js/store/createStoreWithMiddleware.dev.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 import rootSaga from '../sagas';
 
 const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(createSagaMiddleware(rootSaga),
-  logger)(createStore);
+  logger, thunk)(createStore);
 export default createStoreWithMiddleware;
