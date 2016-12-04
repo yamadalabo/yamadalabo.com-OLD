0d476b1a6a7c7853dcdd3ac08083d3042bbd0124
nishitani yuki <yuki.nishitani85@gmail.com>
2016-12-04 09:35:21 +0900

    update actions/sync/news

--- src/js/actions/sync/news.js
+++ src/js/actions/sync/news.js
@@ -1,6 +1,7 @@
 export const START_FETCHING = 'START_FETCHING_NEWS';
 export const SUCCEED_IN_FETCHING = 'SUCCEED_IN_FETCHING_NEWS';
 export const FAIL_TO_FETCH = 'FAIL_TO_FETCH_NEWS';
+export const RESET_ERROR = 'RESET_ERROR';
 
 export function startFetching() {
   return {
@@ -21,5 +22,11 @@ export function failToFetch(error) {
   return {
     type: FAIL_TO_FETCH,
     error,
+  };
+}
+
+export function resetError() {
+  return {
+    type: RESET_ERROR,
   };
 }
