086be8c2848b05f790f56b941a5719fc69ebecce
nishitani yuki <yuki.nishitani85@gmail.com>
2016-12-04 09:30:58 +0900

    bug fix

--- src/js/services/api/helper/generate-query.js
+++ src/js/services/api/helper/generate-query.js
@@ -1,2 +1,2 @@
 export const generateQuery = params => Object.keys(params)
-  .reduce((prevQuery, key) => `${prevQuery}&${key}=${params[key]}`, '');
+  .reduce((prevQuery, key) => `${prevQuery}&${key}=${params[key]}`, '?');
