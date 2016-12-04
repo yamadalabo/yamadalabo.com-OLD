eea8db5910d448048f505db2944e06f994dadaae
nishitani yuki <yuki.nishitani85@gmail.com>
2016-12-04 19:39:59 +0900

    update SeminarPost container

--- src/js/containers/SeminarPost.js
+++ src/js/containers/SeminarPost.js
@@ -1,8 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import findIndex from 'lodash/array/findIndex';
-import moment from 'moment';
-import { loadSeminar, resetSeminar, resetErrorMessage } from '../actions';
+
+import load from '../actions/async/seminar';
+import { resetError } from '../actions/sync/seminar';
 import PageNavigator from '../components/PageNavigator';
 import PostNavigator from '../components/PostNavigator';
 import Post from '../components/Post';
@@ -11,29 +12,18 @@ import ErrorMessage from '../components/ErrorMessage';
 
 class SeminarPost extends Component {
   componentDidMount() {
-    this.handleLoad();
-  }
-
-  handleLoad() {
-    const { updatedAt, errorMessage, isFetching } = this.props;
-    const updatedTime = moment.unix(updatedAt);
-    const now = moment();
-    if (errorMessage) {
-      this.props.resetErrorMessage();
+    const { entities, error } = this.props;
+    if (error !== null) {
+      this.props.resetError();
     }
-    if (!isFetching && (updatedAt === null || now.diff(updatedTime, 'minutes') > 30)) {
-      this.props.resetSeminar();
-      this.props.loadSeminar();
+    if (entities.length === 0) {
+      this.props.load();
     }
-  }
-
-  isEmpty() {
-    const { entities } = this.props;
-    return entities.length === 0;
   }
 
   renderPostNavigator() {
     const { entities, routeParams: { id } } = this.props;
+    // todo: should sort in other place
     const sortedEntities = entities.sort((a, b) => {
       if (a.timestamp > b.timestamp) {
         return -1;
@@ -60,15 +50,15 @@ class SeminarPost extends Component {
   }
 
   renderMainSection() {
-    const { isFetching, errorMessage, entities, routeParams: { id } } = this.props;
+    const { isFetching, error, entities, routeParams: { id } } = this.props;
     if (isFetching) {
       return (
         <Loading />
       );
-    } else if (errorMessage) {
+    } else if (error) {
       return (
         <ErrorMessage
-          message={errorMessage}
+          message={error}
         />
       );
     }
@@ -92,11 +82,13 @@ class SeminarPost extends Component {
   }
 
   render() {
-    const { isFetching } = this.props;
+    const { entities, isFetching } = this.props;
+    const isEmpty = entities.length === 0;
     return (
       <div className="app">
         <PageNavigator />
-        {!isFetching && !this.isEmpty() ? this.renderPostNavigator() : null}
+        {!isFetching && isEmpty ? this.renderPostNavigator() : null}
+        {this.renderPostNavigator()}
         <div className="content">
           {this.renderMainSection()}
         </div>
@@ -106,34 +98,30 @@ class SeminarPost extends Component {
 }
 
 SeminarPost.propTypes = {
-  routeParams: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-  }).isRequired,
-  loadSeminar: PropTypes.func.isRequired,
-  resetSeminar: PropTypes.func.isRequired,
-  resetErrorMessage: PropTypes.func.isRequired,
   entities: PropTypes.arrayOf(PropTypes.shape({
     body: PropTypes.string.isRequired,
     id: PropTypes.number.isRequired,
     timestamp: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
   })).isRequired,
-  updatedAt: PropTypes.number,
+  error: PropTypes.string,
   isFetching: PropTypes.bool.isRequired,
-  errorMessage: PropTypes.string,
+  load: PropTypes.func.isRequired,
+  resetError: PropTypes.func.isRequired,
+  routeParams: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 function mapStateToProps(state) {
   return {
     entities: state.seminar.entities,
-    updatedAt: state.seminar.updatedAt,
     isFetching: state.seminar.isFetching,
-    errorMessage: state.errorMessage,
+    error: state.seminar.error,
   };
 }
 
 export default connect(mapStateToProps, {
-  loadSeminar,
-  resetSeminar,
-  resetErrorMessage,
+  load,
+  resetError,
 })(SeminarPost);
